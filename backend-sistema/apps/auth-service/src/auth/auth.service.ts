4667595771221a3deb947ffd44c562f4276781ef
Roberto Bucio <ra.bucioortega@ugto.mx>
2025-12-02 15:41:04 -0600

    actualizacio

--- backend-sistema/apps/auth-service/src/auth/auth.service.ts
+++ backend-sistema/apps/auth-service/src/auth/auth.service.ts
@@ -13,11 +13,11 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  // 1. Registrar usuario (Encriptar contraseña)
+  // 1. Registrar usuario
   async register(userData: any): Promise<User> {
     const { email, password } = userData;
     const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt); // Requisito: Encriptar
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = this.usersRepository.create({
       email,
@@ -27,13 +27,12 @@ export class AuthService {
     return this.usersRepository.save(newUser);
   }
 
-  // 2. Login (Validar y generar Token de sesión)
+  // 2. Login
   async login(credentials: any) {
     const { email, password } = credentials;
     const user = await this.usersRepository.findOne({ where: { email } });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Generar JWT (Manejo de sesión)
       const payload = { email: user.email, sub: user.id };
       return {
         access_token: this.jwtService.sign(payload),
@@ -42,6 +41,8 @@ export class AuthService {
       throw new UnauthorizedException('Credenciales inválidas');
     }
   }
+
+  // 3. Borrar Usuario (ESTA ES LA FUNCIÓN QUE TE FALTABA)
   async borrarUsuario(email: string) {
     return this.usersRepository.delete({ email });
   }
