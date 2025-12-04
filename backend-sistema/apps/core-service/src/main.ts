9a009a12bc6c6f41b65e6f9bd386ca541e01fc7f
Roberto Bucio <ra.bucioortega@ugto.mx>
2025-12-03 22:30:50 -0600

    actualizacionm

--- backend-sistema/apps/core-service/src/main.ts
+++ backend-sistema/apps/core-service/src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  app.enableCors(); // <--- ¡ESTA LÍNEA ES OBLIGATORIA!
+
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
